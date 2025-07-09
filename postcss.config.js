d6cf4afd765eb328bbaed0286362b7f352c5eb33
Nabeel-UNOPS <nabeels@unops.org>
2025-07-09 23:26:59 +0000

    Fix: Update config file format for CSS build

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-export default {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  }
-  
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
