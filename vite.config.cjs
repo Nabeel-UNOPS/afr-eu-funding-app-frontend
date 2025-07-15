ad853423c416fa38d6f25685f5e65d1e722faa84
Nabeel-UNOPS <65724781+Nabeel-UNOPS@users.noreply.github.com>
2025-07-14 23:31:49 -0400

    Update vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        // The Tailwind config is now passed directly to the plugin
-        tailwindcss({
-          content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-          theme: {
-            extend: {},
-          },
-          plugins: [],
-        }),
-        autoprefixer,
-      ],
-    },
-  },
-})
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react()],
+    })
+    
