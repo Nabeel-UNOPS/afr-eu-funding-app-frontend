4b6033c15fced9d4b2cefe97fb9cdacc72d5d123
Nabeel-UNOPS <65724781+Nabeel-UNOPS@users.noreply.github.com>
2025-07-21 21:49:48 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-    import React from 'react'
+import React from 'react'
     import ReactDOM from 'react-dom/client'
     import App from './App.jsx'
     import './globals.css'
@@ -8,4 +8,3 @@
         <App />
       </React.StrictMode>,
     )
-    
